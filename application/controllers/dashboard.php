f1dc046f14839fe71d845ad46f73ddc198853b07
fanisa22 <fanisaarifah25@gmail.com>
2022-12-09 10:48:03 +0700

    update dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,9 +2,18 @@
 
 class Dashboard extends CI_Controller{
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_dashboard');
+    }
+
     public function index()
     {
         $isi['content'] = 'v_content';
+        $isi['total_buku'] = $this->m_dashboard->total_buku();
+        $isi['total_pengarang'] = $this->m_dashboard->total_pengarang();
+        $isi['total_penerbit'] = $this->m_dashboard->total_penerbit();
         $this->load->view('v_dashboard', $isi);
     }
 }
---
+++ application/models/m_dashboard.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class M_dashboard extends CI_Model {
+
+    public function total_buku()
+    {
+        return $this->db->get('buku')->num_rows();
+    }
+
+    public function total_pengarang()
+    {
+        return $this->db->get('pengarang')->num_rows();
+    }
+
+    public function total_penerbit()
+    {
+        return $this->db->get('penerbit')->num_rows();
+    }
+}
+?>
